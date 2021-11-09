840def808a75e7d2a933bdd278afaf1e788e146a
FinanceNik <niklas.clasen99@gmail.com>
2021-11-09 11:41:19 +0100

    Created a connection from the frontend dropdown menus to the backend function. Next step is to store the information from the frontend to a SQLite Db

--- GUI_TOOL.py
+++ GUI_TOOL.py
@@ -36,6 +36,33 @@ app.layout = html.Div([dcc.Location(id="url"), sidebar, content],
                       style={'backgroundColor': 'white'})
 
 
+@app.callback(
+    dash.dependencies.Output('dd-output-container', 'children'),
+    [dash.dependencies.Input('submit', 'n_clicks'),
+     dash.dependencies.Input('Q_1', 'value'),
+     dash.dependencies.Input('Q_2', 'value'),
+     dash.dependencies.Input('Q_3', 'value'),
+     dash.dependencies.Input('Q_4', 'value'),
+     dash.dependencies.Input('Q_5', 'value'),
+     dash.dependencies.Input('Q_6', 'value'),
+     dash.dependencies.Input('Q_7', 'value'),
+     dash.dependencies.Input('Q_8', 'value'),
+     dash.dependencies.Input('Q_9', 'value')])
+def update_output(n_clicks, value1, value2, value3, value4, value5, value6, value7, value8, value9):
+    print('*********************************************************************')
+    print(n_clicks)
+    print(value1)
+    print(value2)
+    print(value3)
+    print(value4)
+    print(value5)
+    print(value6)
+    print(value7)
+    print(value8)
+    print(value9)
+    print('*********************************************************************')
+
+
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def render_page_content(pathname):
     if pathname == "/":
@@ -70,89 +97,127 @@ def render_content(tab):
                 html.Hr(),
                 html.H5('1. Knowledge about financial markets:'),
                 html.Div(['How often do you inform yourself about the happenings in the financial markets?']),
-                dcc.Dropdown(id='Q_1',
-                             options=[{'label': i, 'value': i} for i in ['never',
-                                                                         'seldom',
-                                                                         'sometimes',
-                                                                         'often',
-                                                                         'very often']]),
+                html.Div([
+                    dcc.Dropdown(id='Q_1',
+                                 options=[{'label': i, 'value': i} for i in ['never',
+                                                                             'seldom',
+                                                                             'sometimes',
+                                                                             'often',
+                                                                             'very often']]),
+                    html.Div(id='dd-output-container')
+                ]),
+
                 html.Hr(),
                 html.H5('2. Knowledge about financial instruments:'),
                 html.Div(['For how many years are you investing in financial instruments of any kind?']),
-                dcc.Dropdown(id='Q_2',
-                             options=[{'label': i, 'value': i} for i in ['< 1',
-                                                                         '1 - 3',
-                                                                         '3 - 5',
-                                                                         '4 - 8',
-                                                                         '> 8']]),
+                html.Div([
+                    dcc.Dropdown(id='Q_2',
+                                 options=[{'label': i, 'value': i} for i in ['< 1',
+                                                                             '1 - 3',
+                                                                             '3 - 5',
+                                                                             '4 - 8',
+                                                                             '> 8']]),
+                    html.Div(id='dd-output-container')
+                ]),
                 html.Hr(),
                 html.H5('3. Risk & return preference:'),
                 html.Div(['Which of the following portfolio returns seem most interesting to you?   [-2 to +2%  |  '
                           '-5 to +5%  |  -10 to +10%  |  -15 to +15%  |  -20 to +20%]']),
-                dcc.Dropdown(id='Q_3',
-                             options=[{'label': i, 'value': i} for i in ['-2 to +2%',
-                                                                         '-5 to +5%',
-                                                                         '-10 to +10%',
-                                                                         '-15 to +15%',
-                                                                         '-20 to +20%']]),
+                html.Div([
+                    dcc.Dropdown(id='Q_3',
+                                 options=[{'label': i, 'value': i} for i in ['-2 to +2%',
+                                                                             '-5 to +5%',
+                                                                             '-10 to +10%',
+                                                                             '-15 to +15%',
+                                                                             '-20 to +20%']]),
+                    html.Div(id='dd-output-container')
+                ]),
+
                 html.Hr(),
                 html.H5('4. Dealing with falling prices:'),
                 html.Div(['What would you do if your portfolio suddenly lost 15% in value?']),
-                dcc.Dropdown(id='Q_4',
-                             options=[{'label': i, 'value': i} for i in ['Liquidate all positions.',
-                                                                         'Liquidate all negative positions.',
-                                                                         'Change my investing strategy.',
-                                                                         'Do nothing. Markets can be volatile.',
-                                                                         'I would by the dip.']]),
+                html.Div([
+                    dcc.Dropdown(id='Q_4',
+                                 options=[{'label': i, 'value': i} for i in ['Liquidate all positions.',
+                                                                             'Liquidate all negative positions.',
+                                                                             'Change my investing strategy.',
+                                                                             'Do nothing. Markets can be volatile.',
+                                                                             'I would by the dip.']]),
+                    html.Div(id='dd-output-container')
+                ]),
                 html.Hr(),
                 html.H5('5. Preference towards risk and return distribution:'),
                 html.Div(['What statement is most closely resembling you investment philosophy?']),
-                dcc.Dropdown(id='Q_5',
-                             options=[{'label': i, 'value': i} for i in ['First and foremost, I want safety and stability.',
-                                                                         'I would take slight risk to increase my return.',
-                                                                         'I take considerable risk to get higher return.',
-                                                                         'I want high return, therefore I accept great risk.',
-                                                                         'All I care about is return, no matter the risk.']]),
+                html.Div([
+                    dcc.Dropdown(id='Q_5',
+                                 options=[{'label': i, 'value': i} for i in
+                                          ['First and foremost, I want safety and stability.',
+                                           'I would take slight risk to increase my return.',
+                                           'I take considerable risk to get higher return.',
+                                           'I want high return, therefore I accept great risk.',
+                                           'All I care about is return, no matter the risk.']]),
+                    html.Div(id='dd-output-container')
+                ]),
                 html.Hr(),
                 html.H2('Assessment of the Client\'s Risk Capacity'),
                 html.Hr(),
                 html.H5('1. Liquidity:'),
                 html.Div(['How large is the portion of your cash equivalent assets to your total net worth?']),
-                dcc.Dropdown(id='Q_6',
-                             options=[{'label': i, 'value': i} for i in ['< 20%',
-                                                                         '20 - 40%',
-                                                                         '40 - 60%',
-                                                                         '60 - 80%',
-                                                                         '> 80%']]),
+                html.Div([
+                    dcc.Dropdown(id='Q_6',
+                                 options=[{'label': i, 'value': i} for i in ['< 20%',
+                                                                             '20 - 40%',
+                                                                             '40 - 60%',
+                                                                             '60 - 80%',
+                                                                             '> 80%']]),
+                    html.Div(id='dd-output-container')
+                ]),
                 html.Hr(),
                 html.H5('2. Investment horizon:'),
                 html.Div(['When would you probably like to access the invested capital?']),
-                dcc.Dropdown(id='Q_7',
-                             options=[{'label': i, 'value': i} for i in ['In less than 2 years.',
-                                                                         'In about 4 years.',
-                                                                         'In about 6 years.',
-                                                                         'Not within 10 years.',
-                                                                         'No time horizon less than 10 years.']]),
+                html.Div([
+                    dcc.Dropdown(id='Q_7',
+                                 options=[{'label': i, 'value': i} for i in ['In less than 2 years.',
+                                                                             'In about 4 years.',
+                                                                             'In about 6 years.',
+                                                                             'Not within 10 years.',
+                                                                             'No time horizon less than 10 years.']]),
+                    html.Div(id='dd-output-container')
+                ]),
                 html.Hr(),
                 html.H5('3. Savings rate:'),
                 html.Div(['What statement best reflects your cost & income situation?']),
-                dcc.Dropdown(id='Q_8',
-                             options=[{'label': i, 'value': i} for i in ['I have higher costs than income.',
-                                                                         'My costs and income are equal.',
-                                                                         'My costs are slightly lower than income.',
-                                                                         'My costs are less than half of my income.',
-                                                                         'My income is significantly higher than my costs.']]),
+                html.Div([
+                    dcc.Dropdown(id='Q_8',
+                                 options=[{'label': i, 'value': i} for i in ['I have higher costs than income.',
+                                                                             'My costs and income are equal.',
+                                                                             'My costs are slightly lower than income.',
+                                                                             'My costs are less than half of my income.',
+                                                                             'My income is significantly higher than my costs.']]),
+                    html.Div(id='dd-output-container')
+                ]),
                 html.Hr(),
                 html.H5('4. Liquidity reserves:'),
                 html.Div(['For how many years would the cash portion of your total net worth '
                           'cover your living expenses if you had no other income?']),
-                dcc.Dropdown(id='Q_9',
-                             options=[{'label': i, 'value': i} for i in ['Less than 2 years.',
-                                                                         '2 to 6 years.',
-                                                                         '6 to 10 years.',
-                                                                         '10 to 15 years.',
-                                                                         'More than 15 years.']]),
+                html.Div([
+                    dcc.Dropdown(id='Q_9',
+                                 options=[{'label': i, 'value': i} for i in ['Less than 2 years.',
+                                                                             '2 to 6 years.',
+                                                                             '6 to 10 years.',
+                                                                             '10 to 15 years.',
+                                                                             'More than 15 years.']]),
+                    html.Div(id='dd-output-container')
+                ]),
                 html.Hr(),
+                html.Div([
+                    html.Button('Click to Submit', id='submit', n_clicks=0)
+                ], style={'width': f'{13}%', 'display': 'inline-block', 'align': 'center', 'padding': '10px',
+                          'margin-left': '0%',
+                          'font-size': '24px',
+                          'box-shadow': '5px 4px 5px 5px lightgrey',
+                          'borderRadius': '10px',
+                          'overflow': 'hidden'})
             ])
         ])
     elif tab == 'tab-2':
