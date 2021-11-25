6c9ec3ae778eaf0386d7c99502954a7b1db0070b
FinanceNik <niklas.clasen99@gmail.com>
2021-11-26 00:14:05 +0100

    worked on the portfolio creation module. Now it takes the csv as data

--- Porter.py
+++ Porter.py
@@ -6,61 +6,80 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 
+# pandas_datareader is a sub-module of pandas that allows us to call finance APIs to get datasets directly from source.
+import pandas_datareader as web
+
+# Datetime is a built-in Python module for working with timed date. In our case we need it to specify the range of the
+# Time series.
+from datetime import datetime
+
 # This is the list of assets that are going to be used. ATM just dummies.
 import Styles
 
+assets_used = ['^GSPC', 'GC=F', 'GOOG']
 
-def data_preparation():
-    df = pd.read_csv("data.csv")
-    df = df.iloc[::-1]
-    print(df)
-    column_list = df.columns[1:]
-    for i in column_list:
-        df.insert(1, f"{i}_Change", "")
-        df[f"{i}_Change"] = df[f"{i}"].pct_change()
-        df.insert(1, f"{i}_LogReturn", "")
-        df[f"{i}_LogReturn"] = np.log(1 + df[f"{i}_Change"])
-        avg_change = df[f"{i}_LogReturn"].mean()
-        df.insert(1, f"{i}_Excess", "")
+
+def data_prepper():
+    start = datetime(2015, 1, 1)
+    end = datetime(2019, 12, 31)
+    tickers = assets_used
+    dfs_raw = []
+    for ticker in tickers:
+        stock = web.DataReader(ticker, "yahoo", start, end)
+        dfs_raw.append(stock)
+    assets = dfs_raw
+    dfs = []
+    for i in range(len(assets)):
+        df_raw = assets[i]
+        df = df_raw.rename(columns={"Adj Close": "Adjusted"})
+        df.insert(1, 'Change', '')
+        df['Change'] = df.Adjusted.pct_change()
+        df.insert(1, 'LogReturn', '')
+        df['LogReturn'] = np.log(1 + df.Change)
+        avg_change = df.LogReturn.mean()
+        df.insert(1, 'Excess', '')
 
         def excess_calculator(x):
             excess = x - avg_change
             return excess
 
-        df[f"{i}_Excess"] = df[f"{i}_LogReturn"].apply(excess_calculator)
-
-    df_excess = df[["Date", "Cash CHF_Excess", "Bonds CHF_Excess", "Bonds FC (hedged)_Excess", "Swiss Equity_Excess",
-                    "Global Equity_Excess", "Global Equity Small Caps_Excess", "EM Equity_Excess", "Real Estate_Excess"]]
-
-    df_raw = df[["Date", "Cash CHF", "Bonds CHF", "Bonds FC (hedged)", "Swiss Equity",
-                 "Global Equity", "Global Equity Small Caps", "EM Equity", "Real Estate"]]
-
-    return df_raw, df_excess
+        df['Excess'] = df['LogReturn'].apply(excess_calculator)
+        dfs.append(df)
+    # print(dfs[0])
+    return dfs
 
 
 # This function creates a portfolio with fixed weights (for now). It is just for testing purposes but it spits out a
 # graph with the efficient frontier of all of the given stocks.
 def optimal_portfolio():
-    df_excess = data_preparation()[1]
-    df_raw = data_preparation()[0]
+    assets = data_prepper()
+    names = assets_used
+    dfs = []
+    for asset in range(len(assets)):
+        df_raw = assets[asset]['Adjusted']
+        df = df_raw.rename(f'{names[asset]}')
+        dfs.append(df)
+    df = pd.concat(dfs, axis=1, ignore_index=False)
 
     def covariance_matrix():
-        return df_excess.cov()
+        dfs_cov = []
+        for i in range(len(assets)):
+            df_cov_raw = assets[i]['Excess']
+            df = df_cov_raw.rename(f'{names[i]}')
+            dfs_cov.append(df)
+        return pd.concat(dfs_cov, axis=1, ignore_index=False).cov()
 
     cov_matrix = covariance_matrix()
-
-    ind_er = []
-    for i in df_raw.columns[1:]:
-        ind_er.append((df_raw[i].pct_change().mean())*12)
-
-    ind_er = pd.Series([x for x in ind_er], index=df_raw.columns[1:])
+    ind_er = df.resample('Y').last().pct_change().mean()
+    print(ind_er)
+    print(type(ind_er))
 
     p_returns = []
     p_volatility = []
     p_weights = []
 
-    num_assets = len(df_raw.columns[1:])
-    num_portfolios = 10000
+    num_assets = len(df.columns)
+    num_portfolios = 1000
 
     for portfolio in range(num_portfolios):
         weights = np.random.random(num_assets)
@@ -70,12 +89,12 @@ def optimal_portfolio():
         p_returns.append(returns)
         var = cov_matrix.mul(weights, axis=0).mul(weights, axis=1).sum().sum()  # Portfolio Variance
         sd = np.sqrt(var)
-        ann_sd = sd * np.sqrt(12)
+        ann_sd = sd * np.sqrt(250)
         p_volatility.append(ann_sd)
 
     data = {'Returns': p_returns, 'Volatility': p_volatility}
 
-    for counter, symbol in enumerate(df_raw.columns[1:].tolist()):
+    for counter, symbol in enumerate(df.columns.tolist()):
         data[symbol + ' weight'] = [w[counter] for w in p_weights]
 
     # These are all the possible portfolio combinations.
@@ -93,6 +112,7 @@ def optimal_portfolio():
     plt.xlabel("Volatility")
     plt.ylabel("Return")
     # plt.savefig("assets/portfolio.png", dpi=130)
-    plt.show()
+    # plt.show()
 
 
+optimal_portfolio()
