711f53df134b3f71117e8291acbc612457367f8a
FinanceNik <niklas.clasen99@gmail.com>
2022-01-03 13:45:00 +0100

    Additions to the backtesting algorithm. Testing the first 6 months of data

--- Backtesting.py
+++ Backtesting.py
@@ -32,7 +32,6 @@ def dataframe_construction():
 
 def dataframe_population_weights():
     df = dataframe_construction()
-    print(df.columns[:])
     data = dh.selected_portfolio_weights()[1]
     data_weights = {"CA": data[0], "BO": data[1], "BOFC": data[2], "SE": data[3],
                     "GE": data[4], "GES": data[5], "EME": data[6], "RE": data[7]}
@@ -45,7 +44,7 @@ def dataframe_population_weights():
     return df
 
 
-def dataframe_population_values():
+def dataframe_population_firstRow():
     df = dataframe_population_weights()
     df['portfolio_value'][177] = 100_000
     df['period_return'][177] = 0.0
@@ -55,7 +54,38 @@ def dataframe_population_values():
     for asset in asset_list:
         df[f'{asset}_share'][177] = float(df[f'{asset}_value'][177]) / float(df[f'{asset}'][177])
 
+    return df
+
+
+def dataframe_population_firstYear():
+    df = dataframe_population_firstRow()
+    for i in range(176, 171, -1):
+        for asset in asset_list:
+            try:
+                df[f'{asset}_value'][i] = float(df[f'{asset}_share'][177]) * float(df[f'{asset}'][i])
+            except:
+                df[f'{asset}_value'][i] = 0.0
+        df['portfolio_value'][i] = float(df['CA_value'][i]) + float(df['BO_value'][i]) + float(df['BOFC_value'][i]) + \
+                                   float(df['SE_value'][i]) + float(df['GE_value'][i]) + float(df['GES_value'][i]) + \
+                                   float(df['EME_value'][i]) + float(df['RE_value'][i])
+        df['portfolio_value'][i] = round(df['portfolio_value'][i], 2)
+
+    for asset in asset_list:
+        try:
+            df[f'{asset}_value'][171] = float(df[f'{asset}_weight'][171]) * float(df['portfolio_value'][171+1])
+        except:
+            df[f'{asset}_value'][171] = 0.0
+
+    for asset in asset_list:
+        df[f'{asset}_share'][171] = float(df[f'{asset}_value'][171]) / float(df[f'{asset}'][171])
+
+    df['portfolio_value'][171] = float(df['CA_value'][171]) + float(df['BO_value'][171]) + float(df['BOFC_value'][171]) + \
+                               float(df['SE_value'][171]) + float(df['GE_value'][171]) + float(df['GES_value'][171]) + \
+                               float(df['EME_value'][171]) + float(df['RE_value'][171])
+    df['portfolio_value'][171] = round(df['portfolio_value'][171], 2)
+
     df.to_csv('XXX.csv')
 
 
-dataframe_population_values()
+dataframe_population_firstYear()
+
