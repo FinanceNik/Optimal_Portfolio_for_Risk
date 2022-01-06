1955d53ed4bb07ae8a64d7b5b56711e7686881c8
FinanceNik <niklas.clasen99@gmail.com>
2022-01-06 16:37:00 +0100

    Changes to the Monte Carlo Analysis

--- Monte_Carlo_Simulation.py
+++ Monte_Carlo_Simulation.py
@@ -4,38 +4,27 @@ import datetime as dt
 import numpy as np
 from matplotlib import pyplot as plt
 
-start = dt.datetime(2017, 1, 3)
-end = dt.datetime(2017, 12, 31)
 
-prices = web.DataReader('AAPL', 'yahoo', start, end)['Close']
-returns = prices.pct_change()
+def monte_carlo_simulation(scenario):
+    # --> Scenario can be: ['bull', 'bear', 'neutral']
+    if scenario == 'bear':
+        expected_returns = 0.03
+    elif scenario == 'bull':
+        expected_returns = 0.11
+    elif scenario == 'neutral':
+        expected_returns = 0.05
 
-last_price = prices[-1]
+    investment = 2_000_000
+    volatility = 0.14
+    duration = 50
+    sip = 10000
+    final_values = []
+    for year in range(duration):
+        market_return = np.random.normal(expected_returns, volatility)
+        final_value = sip + (1 + market_return) * investment
+        investment = final_value
+        final_values.append(int(final_value))
+    print(final_values)
 
-# Number of Simulations
-num_simulations = 1000
-num_days = 252  # <-- Probably have to change this to 12 for months
 
-simulation_df = pd.DataFrame()
-
-for i in range(num_simulations):
-    count = 0
-    daily_vol = returns.std()
-
-    price_series = []
-
-    price = last_price * (1 + np.random.normal(0, daily_vol))
-    price_series.append(price)
-
-    for x in range(num_days):
-        if count == 251:  # <-- Have to change this to 12 too for months
-            break
-        price = price_series[count] * (1 + np.random.normal(0, daily_vol))
-        price_series.append(price)
-        count += 1
-
-    simulation_df[i] = price_series
-
-fig = plt.figure()
-plt.plot(simulation_df)
-plt.show()
+monte_carlo_simulation('neutral')
